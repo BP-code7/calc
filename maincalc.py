99bf0e83ab19b1428c72d24a9026bce9d1796c01
unknown <bishoppoudel@gmail.com>
2025-11-07 21:23:57 +0545

    main file done

--- maincalc.py
+++ maincalc.py
@@ -1,1 +1,16 @@
-#This is by Bishop Poudel.
+import Add
+import sub
+import mul
+import div
+
+a=100
+b=50
+
+
+total_sum=add.add(a,b)
+total_sub=sub.sub(a,b)
+total_mul=mul.mul(a,b)
+total_div=div.div(a,b)
+
+print(f"total sum is {total_sum}. total sub is {total_sub}. total mul 
+is{total_mul}. total div is{total_div}.")
