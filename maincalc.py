12194e30d32089c6516d803790f5c37a6e7365a6
unknown <bishoppoudel@gmail.com>
2025-11-08 08:57:15 +0545

    main file done

--- maincalc.py
+++ maincalc.py
@@ -1,4 +1,4 @@
-import Add
+import add
 import sub
 import mul
 import div
@@ -7,10 +7,9 @@ a=100
 b=50
 
 
-total_sum=add.add(a,b)
-total_sub=sub.sub(a,b)
-total_mul=mul.mul(a,b)
-total_div=div.div(a,b)
+total_sum = add.add(a,b)
+total_sub = sub.sub(a,b)
+total_mul = mul.mul(a,b)
+total_div = div.div(a,b)
 
-print(f"total sum is {total_sum}. total sub is {total_sub}. total mul 
-is{total_mul}. total div is{total_div}.")
+print(f"total sum is {total_sum}. total sub is {total_sub}. total mul is{total_mul}. total div is{total_div}")
